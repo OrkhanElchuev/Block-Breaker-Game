d7ff1d7beb94b6e32fb381d45f222bee98ab1355
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-13 22:31:09 +0200

    fixed bug with the paddle glitching in walls

--- Assets/Scritps/Paddle.cs
+++ Assets/Scritps/Paddle.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     // Configuration parameters
-    [SerializeField] float minX = -2f;
-    [SerializeField] float maxX = 17f;
-    [SerializeField] float screenWidthInUnits = 20f;
+    [SerializeField] float normalizedPositionLeftX = 1.0f;
+    [SerializeField] float normalizedPositionRightX = 14.7f;
+    [SerializeField] float normalizedPositionY = 0.28f;
+    [SerializeField] float minX = 0.5f;
+    [SerializeField] float maxX = 15.0f;
 
-    // cached references
+    // Variables
     GameStatus theGameSession;
     Ball theBall;
+    GameObject paddlePos;
 
     // Start is called before the first frame update
     void Start()
     {
         theGameSession = FindObjectOfType<GameStatus>();
         theBall = FindObjectOfType<Ball>();
+    }
+
+    // To avoid paddle from getting stuck in the walls
+    void Update()
+    {
+        // if position is out of boundaries then bring the paddle back to the play board
+       if(transform.position.x < minX)
+        {
+          transform.position = new Vector2(normalizedPositionLeftX, normalizedPositionY);
+        }
+       
+       if(transform.position.y > maxX)
+        {
+            transform.position = new Vector2(normalizedPositionRightX, normalizedPositionY);
+        }
     }
 }
