670c9045a5b4b1f6e4278dd06fb4a11351031412
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-15 01:35:37 +0200

    Congratulation scene is created

---
+++ Assets/Scritps/CongratsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CongratsMenu : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+}
+
--- Assets/Scritps/FinalLevel.cs
+++ Assets/Scritps/FinalLevel.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class FinalLevel : MonoBehaviour
 {
+    private int lastLevelScore = 9178;
+
     // On destroy of last level load "Congratulations" scene
     private void OnDestroy()
     {
-        SceneManager.LoadScene("Final Level Menu");
+        // Check if level is passed (9178) is the score for the last level
+        if(PlayerPrefs.GetInt("passedLevelScore") >= lastLevelScore)
+        {
+            SceneManager.LoadScene("Final Level Menu");
+        }
     }
 }
