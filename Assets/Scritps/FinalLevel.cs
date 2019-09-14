058dd2a3ae5b041eb459fb16f5dd1e53e276f4ea
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-15 00:52:25 +0200

    All levels are done

---
+++ Assets/Scritps/FinalLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalLevel : MonoBehaviour
+{
+    // On destroy of last level load "Congratulations" scene
+    private void OnDestroy()
+    {
+        SceneManager.LoadScene("Final Level Menu");
+    }
+}
